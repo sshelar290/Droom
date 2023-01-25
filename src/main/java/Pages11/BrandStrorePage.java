4654c0932138b7f1cd33996c299da3afdbda4c98
sshelar290 <sshelar290>
2023-01-25 19:19:15 +0530

    same day updated file

--- src/main/java/Pages11/BrandStrorePage.java
+++ src/main/java/Pages11/BrandStrorePage.java
@@ -13,38 +13,48 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
-
-public class BrandStrore1 {
-	private final static Logger LOG = Logger.getLogger(BrandStrore1.class);
+public class BrandStrorePage {
+	private final static Logger LOG = Logger.getLogger(BrandStrorePage.class);
 
 	@FindBy(css = "div.services-links>a:nth-of-type(4)")
- WebElement brandclick;
+	WebElement brandclick;
 	@FindBy(css = "a[href*=\"/hondacar\"]")
-	 WebElement brand;
+	WebElement brand;
+	@FindBy(xpath = "//a[@class=\"d-display-inline-block d-valign-top gtmEvent\"]")
+	WebElement cardclick;
+	@FindBy(xpath = "//h4[text()=\"Your Cart is Empty\"]")
+	WebElement emptycheck;
 
-	public BrandStrore1() {
+	public BrandStrorePage() {
 		PageFactory.initElements(Base1.driver, this);
 	}
 
-	public void productSearch() throws InterruptedException {
-		Thread.sleep(3000);
+	public void productSearch() {
+
 		brandclick.click();
 		brand.click();
-		
+
 	}
 
-	public static void verifyProduct() throws InterruptedException {
+	public static void verifyProduct() {
 		Set<String> Window_no = Base1.driver.getWindowHandles();
 		String Current_window = Base1.driver.getWindowHandle();
-		for(String s:Window_no) {
-			if(!s.equals(Current_window))
+		for (String s : Window_no) {
+			if (!s.equals(Current_window))
 				Base1.driver.switchTo().window(s);
 		}
-		LOG.info("Title of Page" +Base1.driver.getTitle().toLowerCase());
+		LOG.info("Title of Page" + Base1.driver.getTitle().toLowerCase());
 		Assert.assertTrue(Base1.driver.getTitle().toLowerCase().contains("honda"), "unlisted product");
-		
+
 		LOG.info("Listed product is Honda");
 	}
 
-	
+	public void cartCheack() {
+		Base1.driver.manage().window().maximize();
+		cardclick.click();
+		Assert.assertTrue(emptycheck.isDisplayed(), "Your Cart is not empty");
+		Assert.assertFalse(emptycheck.isDisplayed(), "Your cart is empty");
+
+	}
+
 }
--- src/main/java/Pages11/VehicalinfoPage.java
+++ src/main/java/Pages11/VehicalinfoPage.java
@@ -5,16 +5,16 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class Vehicalinfo {
-	
-	private final static Logger LOG = Logger.getLogger(BrandStrore1.class);
-	@FindBy(xpath ="(//a[text()=\"Vehicle Finder\"])[1]")
-	WebElement vehicle_Finder_label;
-	
+public class VehicalinfoPage {
 
-	public Vehicalinfo() {
+	private final static Logger LOG = Logger.getLogger(VehicalinfoPage.class);
+	@FindBy(xpath = "(//a[text()=\"Vehicle Finder\"])[1]")
+	WebElement vehicle_Finder_label;
+
+	public VehicalinfoPage() {
 		PageFactory.initElements(Base1.driver, this);
 	}
+
 	public void clickable() {
 		vehicle_Finder_label.click();
 
